c17747f71c0fe5622fc01833b359992467e6ca32
nickonometry <nickonometry@gmail.com>
2024-10-30 13:53:12 -0400

    chore: removed link on dashboard

--- src/routes/(app)/dashboard/+page.svelte
+++ src/routes/(app)/dashboard/+page.svelte
@@ -4,20 +4,16 @@
   import InstrumentSets from '../../../lib/components/InstrumentSets.svelte';
 </script>
 
-<div class="grid p-6 gap-6 grid-cols-[1fr_1fr]">
+<div class="grid p-4 gap-6 grid-cols-[1fr_1fr]">
   <Card size="none">
     <CirclePlusOutline class="w-10 h-10 mb-3 text-gray-500 dark:text-gray-400" />
-    <a href="/">
-      <h5 class="mb-2 text-1xl font-semibold tracking-tight text-gray-900 dark:text-white">New order</h5>
-    </a>
+    <h5 class="mb-2 text-1xl font-semibold tracking-tight text-gray-900 dark:text-white">New order</h5>
     <p class="mb-3 font-normal text-gray-500 dark:text-gray-400">Place a new order</p>
   </Card>
 
   <Card size="none">
     <ArrowsRepeatOutline class="w-10 h-10 mb-3 text-gray-500 dark:text-gray-400" />
-    <a href="/">
-      <h5 class="mb-2 text-1xl font-semibold tracking-tight text-gray-900 dark:text-white">Recent cases</h5>
-    </a>
+    <h5 class="mb-2 text-1xl font-semibold tracking-tight text-gray-900 dark:text-white">Recent cases</h5>
     <p class="mb-3 font-normal text-gray-500 dark:text-gray-400">Orders placed in the last 30 days</p>
   </Card>
 
