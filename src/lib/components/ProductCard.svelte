64cd9de2cce9773bda9db490fa568ff9e5299b59
nickonometry <nickonometry@gmail.com>
2024-10-30 12:05:42 -0400

    chore: created a responsive products grid

---
+++ src/lib/components/ProductCard.svelte
@@ -0,0 +1,17 @@
+<script>
+  import { Card, Button } from 'flowbite-svelte';
+  export let imageUrl = '';
+  export let productName = '';
+</script>
+
+<Card padding="none" size="none">
+  <img class="p-8 max-h-56 object-contain rounded-t-lg" src={imageUrl} alt="product 1" />
+  <div class="px-5 pb-5">
+    <div class="flex justify-center items-center flex-col gap-6">
+      <a href="/">
+        <h5 class="text-xl font-semibold tracking-tight text-gray-900 dark:text-white">{productName}</h5>
+      </a>
+      <Button href="/">Customize</Button>
+    </div>
+  </div>
+</Card>
--- src/routes/(app)/products/+page.svelte
+++ src/routes/(app)/products/+page.svelte
@@ -1,1 +1,48 @@
-Products page
+<script>
+  import ProductCard from '$lib/components/ProductCard.svelte';
+
+  let products = [
+    {
+      name: 'Cervical',
+      imageUrl: 'https://nexxtspine.com/wp-content/uploads/2019/06/untitled.89.png'
+    },
+    {
+      name: 'SA Cervical',
+      imageUrl: 'https://nexxtspine.com/wp-content/uploads/2023/08/sa-cervical-square.png'
+    },
+    {
+      name: 'TLIF',
+      imageUrl: 'https://nexxtspine.com/wp-content/uploads/2019/06/untitled.88.png'
+    },
+    {
+      name: 'TLIF Oblique',
+      imageUrl: 'https://nexxtspine.com/wp-content/uploads/2019/06/untitled.87.png'
+    },
+    {
+      name: 'ALIF',
+      imageUrl: 'https://nexxtspine.com/wp-content/uploads/2021/02/Profile-.16-Copy-2.png'
+    },
+    {
+      name: 'SA ALIF',
+      imageUrl: 'https://nexxtspine.com/wp-content/uploads/2022/01/SA-ALIF-Image-NO-Shadow-e1642776756555.png'
+    },
+    {
+      name: 'Lateral',
+      imageUrl: 'https://nexxtspine.com/wp-content/uploads/2021/01/Hero.11-Copy-Copy.png'
+    },
+    {
+      name: 'Corpectomy',
+      imageUrl: 'https://nexxtspine.com/wp-content/uploads/2020/04/Corpectomy.182.png'
+    },
+    {
+      name: 'VBR',
+      imageUrl: 'https://nexxtspine.com/wp-content/uploads/2019/06/untitled.90-1.png'
+    }
+  ];
+</script>
+
+<div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 p-4 w-full">
+  {#each products as product}
+    <ProductCard imageUrl={product.imageUrl} productName={product.name} />
+  {/each}
+</div>
