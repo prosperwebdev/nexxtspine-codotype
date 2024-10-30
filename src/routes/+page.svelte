458766f109fa75d8281aec271bc541403caefcc2
nickonometry <nickonometry@gmail.com>
2024-10-30 08:14:54 -0400

    fixed max width issue on login screen

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,6 +2,6 @@
   import LoginForm from '$lib/components/LoginForm.svelte';
 </script>
 
-<div class="flex flex-col items-center justify-center h-full">
+<div class="flex flex-col items-center justify-center h-full max-w-[768px] m-auto">
   <LoginForm />
 </div>
