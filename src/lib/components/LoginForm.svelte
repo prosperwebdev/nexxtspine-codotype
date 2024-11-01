39d7beee0703eeb013b2b46cf2e0a9f986dc07d7
nickonometry <nickonometry@gmail.com>
2024-11-01 09:20:33 -0400

    chore: fixed an issue with the login form

--- src/lib/components/LoginForm.svelte
+++ src/lib/components/LoginForm.svelte
@@ -26,31 +26,24 @@
       <div class="w-full flex justify-center mb-12">
         <img class="mr-3 h-16 sm:h-12" src="/logo.png" alt="logo" />
       </div>
-      <form class="flex flex-col space-y-6" action="/">
+      <div class="flex flex-col space-y-6">
         <Label class="space-y-2">
           <span>Your email</span>
-          <Input type="email" name="email" bind:value={emailValue} required />
+          <Input type="text" name="email" bind:value={emailValue} required />
         </Label>
         <Label class="space-y-2">
           <span>Your password</span>
-          <Input type="password" name="password" required bind:value={passwordValue} />
+          <Input type="text" name="password" required bind:value={passwordValue} />
         </Label>
         <div class="flex items-start">
           <Checkbox>Remember me</Checkbox>
-          <a href="/" class="ml-auto text-sm text-blue-700 hover:underline dark:text-blue-500"
-            >Forgot password?</a
-          >
+          <a href="/" class="ml-auto text-sm text-blue-700 hover:underline dark:text-blue-500">Forgot password?</a>
         </div>
-        <Button type="submit" class="w-full1" on:click={routeToPage} disabled={!isValid}
-          >Sign in</Button
-        >
+        <Button type="submit" class="w-full1" on:click={routeToPage} disabled={!isValid}>Sign in</Button>
         <p class="text-sm font-light text-gray-500 dark:text-gray-400">
-          Don’t have an account yet? <a
-            href="#"
-            class="font-medium text-primary-600 hover:underline dark:text-primary-500">Sign up</a
-          >
+          Don’t have an account yet? <a href="#" class="font-medium text-primary-600 hover:underline dark:text-primary-500">Sign up</a>
         </p>
-      </form>
+      </div>
     </div>
   </Register>
 </Section>
