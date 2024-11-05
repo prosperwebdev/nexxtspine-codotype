321864024da8bc3e2c2c07d8f3c739259b288906
nickonometry <nickonometry@gmail.com>
2024-11-05 16:26:04 -0500

    chgore: more tweaks for the demo

--- src/lib/components/Toolbar.svelte
+++ src/lib/components/Toolbar.svelte
@@ -1,11 +1,45 @@
 <script>
-  import { ArrowLeftOutline } from 'flowbite-svelte-icons';
+  import { Toolbar, ToolbarButton, Card, Modal, Avatar, Button } from 'flowbite-svelte';
+  import { ArrowLeftOutline, DotsVerticalOutline, FileDocOutline, NewspaperOutline, ExclamationCircleOutline } from 'flowbite-svelte-icons';
   export let title;
+  let modalOpen = false;
 </script>
 
-<div class="h-12 bg-slate-800 p-3">
+<Toolbar>
   <div class="flex flex-row gap-4 items-center">
     <ArrowLeftOutline class="w-7 h-7 text-white" />
     <h1 class="text-sm text-gray-900 sm:text-2xl dark:text-white flex-0">{title}</h1>
   </div>
-</div>
+  <ToolbarButton slot="end" on:click={() => (modalOpen = true)}><DotsVerticalOutline class="w-6 h-6" /></ToolbarButton>
+  <Modal title="Stand Alone Alif Downloads" bind:open={modalOpen} autoclose>
+    <div class="grid grid-cols-1 gap-4">
+      <Card padding="sm" class="w-full min-w-full">
+        <div class="flex flex-col items-center gap-3">
+          <Avatar size="lg"><FileDocOutline class="w-12 h-12" /></Avatar>
+          <span class="text-sm text-gray-500 dark:text-gray-400">STF</span>
+          <div class="flex space-x-3 rtl:space-x-reverse lg:mt-6">
+            <Button color="light" class="dark:text-white">Download</Button>
+          </div>
+        </div>
+      </Card>
+      <Card padding="sm" class="w-full min-w-full">
+        <div class="flex flex-col items-center gap-3">
+          <Avatar size="lg"><NewspaperOutline class="w-12 h-12" /></Avatar>
+          <span class="text-sm text-gray-500 dark:text-gray-400">Brochure</span>
+          <div class="flex space-x-3 rtl:space-x-reverse lg:mt-6">
+            <Button color="light" class="dark:text-white">Download</Button>
+          </div>
+        </div>
+      </Card>
+      <Card padding="sm" class="w-full min-w-full">
+        <div class="flex flex-col items-center gap-3">
+          <Avatar size="lg"><ExclamationCircleOutline class="w-12 h-12" /></Avatar>
+          <span class="text-sm text-gray-500 dark:text-gray-400">IFU</span>
+          <div class="flex space-x-3 rtl:space-x-reverse lg:mt-6">
+            <Button color="light" class="dark:text-white">Download</Button>
+          </div>
+        </div>
+      </Card>
+    </div>
+  </Modal>
+</Toolbar>
